ba57ea449946fd2987a9882c3a65077ed7785a89
just1103 <hyojuson1103@gmail.com>
2022-01-11 10:25:49 +0900

    refactor: ProductPage의 vendor, image 프로퍼티 삭제
    - Vendor, ProductImage 타입 및 파일 삭제
    - 해당 프로퍼티가 필요할 때 추가할 예정
    - 프로퍼티명 수정 (productsInPage -> products)

--- OpenMarket/OpenMarket/Model/Product.swift
+++ OpenMarket/OpenMarket/Model/Product.swift
@@ -10,15 +10,6 @@ struct Product: Codable {
     let bargainPrice: Int
     let discountedPrice: Int
     let stock: Int
-    let images: [ProductImage]?
-    let vendor: Vendor?
     let createdAt: String
     let issuedAt: String
-    
-    enum CodingKeys: String, CodingKey {
-        case id, vendorId, name, thumbnail, currency, price, bargainPrice,
-             discountedPrice, stock, images, createdAt, issuedAt
-        
-        case vendor = "vendors"
-    }
 }
--- OpenMarket/OpenMarket/Model/ProductPage.swift
+++ OpenMarket/OpenMarket/Model/ProductPage.swift
@@ -6,7 +6,7 @@ struct ProductPage: Codable {
     let totalCount: Int
     let offset: Int
     let limit: Int
-    let productsInPage: [Product]
+    let products: [Product]
     let lastPage: Int
     let hasNextPage: Bool
     let hasPrevPage: Bool
@@ -14,7 +14,7 @@ struct ProductPage: Codable {
     enum CodingKeys: String, CodingKey {
         case itemsPerPage, totalCount, offset, limit, lastPage
 
-        case productsInPage = "pages"
+        case products = "pages"
         case pageNumber = "pageNo"
         case hasNextPage = "hasNext"
         case hasPrevPage = "hasPrev"
