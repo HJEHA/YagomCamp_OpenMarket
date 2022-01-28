0e577c205e1e1613e7fcb3b2a0b462f45ab5dff7
hwangjeha <hyhpwang@gmail.com>
2022-01-28 15:41:49 +0900

    feat: 상품 정보 Label, TextView 추가 #8

--- OpenMarket/OpenMarket/View/ProductDetailView/ProductDetailScrollView.swift
+++ OpenMarket/OpenMarket/View/ProductDetailView/ProductDetailScrollView.swift
@@ -4,7 +4,7 @@ class ProductDetailScrollView: UIScrollView {
     private let verticalStackView = UIStackView()
     private var productDetailImageCollectionView = UICollectionView(frame: CGRect.zero,
                                                                     collectionViewLayout: UICollectionViewFlowLayout())
-    private let imageNumberPageControl = UIPageControl()
+    private(set) var imageNumberPageControl = UIPageControl()
     private let nameLabel = UILabel()
     private let stockLabel = UILabel()
     private let bargainPriceLabel = UILabel()
@@ -27,7 +27,10 @@ class ProductDetailScrollView: UIScrollView {
     func setupSubviews() {
         setupVerticalStackView()
         setupProductDetailImageCollectionView()
-        
+        setPageControl()
+        setLabelsStackView()
+        setLabels()
+        setupDescriptionTextView()
         setupSubviewsAutoresizingMask()
     }
     
@@ -37,7 +40,7 @@ class ProductDetailScrollView: UIScrollView {
         verticalStackView.alignment = .fill
         verticalStackView.distribution = .fill
         verticalStackView.spacing = 10
-        verticalStackView.backgroundColor = .systemBlue
+        verticalStackView.backgroundColor = .systemRed
         let inset: CGFloat = 15
         verticalStackView.layoutMargins = UIEdgeInsets(top: inset, left: inset, bottom: inset, right: inset)
         verticalStackView.isLayoutMarginsRelativeArrangement = true
@@ -61,6 +64,67 @@ class ProductDetailScrollView: UIScrollView {
 
         productDetailImageCollectionView.heightAnchor.constraint(equalTo: productDetailImageCollectionView.widthAnchor)
             .isActive = true
+    }
+    
+    func setPageControl() {
+        verticalStackView.addArrangedSubview(imageNumberPageControl)
+        imageNumberPageControl.numberOfPages = 5 // todo 밖에서 이미지 개수 설정
+        imageNumberPageControl.currentPage = 0
+        imageNumberPageControl.pageIndicatorTintColor = .lightGray
+        imageNumberPageControl.currentPageIndicatorTintColor = .systemBlue
+    }
+    
+    func setLabelsStackView() {
+        let horizontalStackView = UIStackView()
+        horizontalStackView.translatesAutoresizingMaskIntoConstraints = false
+        horizontalStackView.axis = .horizontal
+        horizontalStackView.distribution = .fill
+        verticalStackView.addArrangedSubview(horizontalStackView)
+        horizontalStackView.addArrangedSubview(nameLabel)
+        horizontalStackView.backgroundColor = .systemGray
+        
+        let stockAndPriceStackView = UIStackView()
+        stockAndPriceStackView.translatesAutoresizingMaskIntoConstraints = false
+        stockAndPriceStackView.axis = .vertical
+        stockAndPriceStackView.alignment = .trailing
+        stockAndPriceStackView.distribution = .fill
+       
+        horizontalStackView.addArrangedSubview(stockAndPriceStackView)
+        stockAndPriceStackView.addArrangedSubview(stockLabel)
+        stockAndPriceStackView.addArrangedSubview(priceLabel)
+        stockAndPriceStackView.addArrangedSubview(bargainPriceLabel)
+        stockAndPriceStackView.backgroundColor = .systemPink
+        stockAndPriceStackView.setContentCompressionResistancePriority(.required, for: .horizontal)
+    }
+    
+    func setLabels() {
+        nameLabel.text = "제목제목제목제목제목제목제목제목제목제목제목"
+        nameLabel.font = .preferredFont(forTextStyle: .body)
+        nameLabel.textAlignment = .left
+        nameLabel.numberOfLines = 0
+          
+        stockLabel.text = "남은 수량 : 182"
+        stockLabel.font = .preferredFont(forTextStyle: .body)
+        stockLabel.textAlignment = .right
+        stockLabel.setContentCompressionResistancePriority(.required, for: .horizontal)
+        
+        priceLabel.text = "KRW 20000"
+        priceLabel.font = .preferredFont(forTextStyle: .body)
+        priceLabel.textAlignment = .right
+        
+        bargainPriceLabel.text = "KRW 10000"
+        bargainPriceLabel.font = .preferredFont(forTextStyle: .body)
+        bargainPriceLabel.textAlignment = .right
+    }
+    
+    private func setupDescriptionTextView() {
+        verticalStackView.addArrangedSubview(descriptionTextView)
+        descriptionTextView.text = ProductPlaceholder.description.text
+        descriptionTextView.textColor = UIColor.lightGray
+        descriptionTextView.isEditable = false
+        descriptionTextView.isScrollEnabled = false
+        descriptionTextView.font = .preferredFont(forTextStyle: .body)
+        descriptionTextView.setContentHuggingPriority(.defaultLow, for: .vertical)
     }
     
     private func setupSubviewsAutoresizingMask() {
